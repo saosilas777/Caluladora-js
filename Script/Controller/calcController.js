c35f8bfdae709d3fd4a4be15ebf45112f6bf50c0
Silas Dias <saosilas@hotmail.com>
2023-04-05 16:41:07 -0300

    aplicando eventos a calculadora

---
+++ Script/Controller/calcController.js
@@ -0,0 +1,54 @@
+class CalcController {
+
+    constructor() {
+        this._locale = 'pt-BR'
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl = document.querySelector('#data');
+        this._timeEl = document.querySelector('#hora');
+        this._currentDate;
+        this.init();
+        this.initButtonsEvents();
+    };
+
+    //recupera um valor do HTML
+    get displayCalc() { return this._displayCalcEl.innerHTML; };
+    //Seta um valor do HTML
+    set displayCalc(value) { this._displayCalcEl.innerHTML = value };
+
+    get dataAtual() { return this._currentDate; };
+    set dataAtual(value) { this._currentDate = value };
+
+    get displayTime() { return this._timeEl.innerHTML };
+    set displayTime(value) { this._timeEl.innerHTML = value };
+
+    get displayDate() { return this._dateEl.innerHTML };
+    set displayDate(value) { this._dateEl.innerHTML = value };
+
+    get currentDate() { return new Date() };
+    set currentDate(value) { this._currentDate.innerHTML = value };
+
+
+    init() {
+        this.setDisplayDateTime();
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);
+
+    };
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('#buttons > g , #parts > g');
+        
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e=>{
+                console.log(btn.className.baseVal.replace('btn-',''))
+            })
+        });
+    };
+
+    setDisplayDateTime() {
+        // this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day:'2-digit', month:'short', year:'numeric'});
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+}
